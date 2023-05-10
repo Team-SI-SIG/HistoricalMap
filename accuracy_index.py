131d0039af77de3231e22ad47917b8a71f776800
Julien Monticolo <julien.monticolo@conservatoire-sites-alsaciens.eu>
2023-05-10 19:20:52 +0200

    [Migrate] accuracy_index.py to QGIS3

--- accuracy_index.py
+++ accuracy_index.py
@@ -1,32 +1,37 @@
-'''!@brief Accuracy index by Mathieu Fauvel
-'''
+#! /usr/bin/python3
+
+"""!@brief Accuracy index by Mathieu Fauvel
+"""
+from __future__ import annotations
+
 import scipy as sp
+
 
 class CONFUSION_MATRIX:
     def __init__(self):
-        self.confusion_matrix=None
-        self.OA=None
+        self.confusion_matrix = None
+        self.OA = None
         self.Kappa = None
-        
-    def compute_confusion_matrix(self,yp,yr):
-        ''' 
+
+    def compute_confusion_matrix(self, yp, yr):
+        """
         Compute the confusion matrix
-        '''
+        """
         # Initialization
         n = yp.size
-        C=int(yr.max())
-        self.confusion_matrix=sp.zeros((C,C))
-        
+        C = int(yr.max())
+        self.confusion_matrix = sp.zeros((C, C))
+
         # Compute confusion matrix
         for i in range(n):
-            self.confusion_matrix[yp[i].astype(int)-1,yr[i].astype(int)-1] +=1
-        
+            self.confusion_matrix[yp[i].astype(int) - 1, yr[i].astype(int) - 1] += 1
+
         # Compute overall accuracy
-        self.OA=sp.sum(sp.diag(self.confusion_matrix))/n
-        
+        self.OA = sp.sum(sp.diag(self.confusion_matrix)) / n
+
         # Compute Kappa
-        nl = sp.sum(self.confusion_matrix,axis=1)
-        nc = sp.sum(self.confusion_matrix,axis=0)
-        self.Kappa = ((n**2)*self.OA - sp.sum(nc*nl))/(n**2-sp.sum(nc*nl))
-        
+        nl = sp.sum(self.confusion_matrix, axis=1)
+        nc = sp.sum(self.confusion_matrix, axis=0)
+        self.Kappa = ((n**2) * self.OA - sp.sum(nc * nl)) / (n**2 - sp.sum(nc * nl))
+
         # TBD Variance du Kappa
